c9d0a76fa300faa8b9fbddc3427960002fba75f1
Matthias Becker <mabecker@kth.se>
2024-04-18 20:46:25 +0200

    Updated location of example projects and folder for generated projects.

--- CodeGeneration/src/mabecker/qnx/codegeneration/Main.java
+++ CodeGeneration/src/mabecker/qnx/codegeneration/Main.java
@@ -28,43 +28,43 @@ public class Main {
 		
 		switch (selectedExperiment) {
 		case WATERS2019_PARTITIONED:
-			inputFolder = "model_input_WATERS2019";
+			inputFolder = "../Examples/model_input_WATERS2019";
 			modelName = "WATERS2019";
 			break;
 		case WATERS2019_GLOBAL:
-			inputFolder = "model_input_WATERS2019_Global";
+			inputFolder = "../Examples/model_input_WATERS2019_Global";
 			modelName = "WATERS2019_Global";
 			break;
 		case WATERS2019_APS:
-			inputFolder = "model_input_WATERS2019_APS";
+			inputFolder = "../Examples/model_input_WATERS2019_APS";
 			modelName = "WATERS2019_APS";
 			break;
 		case BBW_SINGLE_NODE_FPPS:
-			inputFolder = "model-input_local";
+			inputFolder = "../Examples/model-input_local";
 			modelName = "RPI_BBW_local";
 			break;
 		case BBW_DISTRIBUTED_FPPS:
-			inputFolder = "model-input_distributed";
+			inputFolder = "../Examples/model-input_distributed";
 			modelName = "RPI_BBW_distributed";
 			break;
 		case BBW_SINGLE_GLOBALSCHED_1CPU:
-			inputFolder = "model-input_local_1core";
+			inputFolder = "../Examples/model-input_local_1core";
 			modelName = "RPI_BBW_local_1core";
 			break;
 		case BBW_SINGLE_GLOBALSCHED_2CPU:
-			inputFolder = "model-input_local_2core";
+			inputFolder = "../Examples/model-input_local_2core";
 			modelName = "RPI_BBW_local_2core";
 			break;
 		case BBW_SINGLE_GLOBALSCHED_3CPU:
-			inputFolder = "model-input_local_3core";
+			inputFolder = "../Examples/model-input_local_3core";
 			modelName = "RPI_BBW_local_3core";
 			break;
 		case BBW_SINGLE_GLOBALSCHED_4CPU:
-			inputFolder = "model-input_local_4core";
+			inputFolder = "../Examples/model-input_local_4core";
 			modelName = "RPI_BBW_local_4core";
 			break;
 		case TEST:
-			inputFolder = "model-input_test";
+			inputFolder = "../Examples/model-input_test";
 			modelName = "Test_Model";
 			break;
 		default:
@@ -82,7 +82,7 @@ public class Main {
 		/**
 		 * Generate the code for each node of the edge system.
 		 */
-		Generator.generateCode(system, "TargetProject/" + modelName, COMPILE, TRANSFER);
+		Generator.generateCode(system, "../TargetProject/" + modelName, COMPILE, TRANSFER);
 	}
 	
 }
